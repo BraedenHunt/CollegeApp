fdeb4270858902e91e05b9ca95287919d358c1db
bjhunt7520 <20hunbra@student.mcpsmt.org>
2017-12-20 10:37:19 -0700

    Initial commit

---
+++ app/src/main/java/org/pltw/examples/collegeapp/Profile.java
@@ -0,0 +1,26 @@
+package org.pltw.examples.collegeapp;
+
+/**
+ * Created by 20hunbra on 12/20/2017.
+ */
+
+public class Profile {
+    private String firstName;
+    private String lateName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLateName() {
+        return lateName;
+    }
+
+    public void setLateName(String lateName) {
+        this.lateName = lateName;
+    }
+}
